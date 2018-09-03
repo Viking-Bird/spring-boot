8cde4f09a2ddeac2863c43a313b74471f18a361d
dapeng <1755610380@qq.com>
2018-09-03 09:57:22 +0800

    Delete spring-boot-gradle-plugin/src/test/java and add spring-boot-example

---
+++ spring-boot-project/spring-boot-example/src/main/java/org/springframework/boot/example/Application.java
@@ -0,0 +1,17 @@
+package org.springframework.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by wangpeng
+ * Date: 2018/9/1
+ * Time: 18:20
+ */
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
