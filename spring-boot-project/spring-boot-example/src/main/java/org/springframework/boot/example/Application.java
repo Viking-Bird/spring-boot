56b85f2556c9aea154f8ee53af2376c6168683a8
dapeng <1755610380@qq.com>
2018-09-04 09:48:36 +0800

    Add unit tests and source logs

--- spring-boot-project/spring-boot-example/src/main/java/org/springframework/boot/example/Application.java
+++ spring-boot-project/spring-boot-example/src/main/java/org/springframework/boot/example/Application.java
@@ -2,6 +2,8 @@ package org.springframework.boot.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by wangpeng
@@ -12,6 +14,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		AnnotationConfigServletWebServerApplicationContext context = (AnnotationConfigServletWebServerApplicationContext) SpringApplication.run(Application.class, args);
+		String[] beanNames = context.getBeanDefinitionNames();
+
+		System.out.println("所以beanNames个数：" + beanNames.length);
+
+		for (String bn : beanNames) {
+			System.err.println(bn);
+		}
+
+		beanNames =  context.getBeanNamesForAnnotation(Repository.class);
+		System.out.println("Repository注解beanNames个数："+beanNames.length);
+
+		for(String bn:beanNames){
+			System.err.println(bn);
+		}
 	}
 }
---
+++ spring-boot-project/spring-boot-example/src/test/java/org/springframework/boot/example/test/SourceTest.java
@@ -0,0 +1,17 @@
+package org.springframework.boot.example.test;
+
+import org.junit.Test;
+import org.springframework.boot.web.servlet.context.AnnotationConfigServletWebServerApplicationContext;
+
+/**
+ * Created by wangpeng
+ * Date: 2018/9/3
+ * Time: 19:45
+ */
+public class SourceTest {
+
+	@Test
+	public void test1() {
+		AnnotationConfigServletWebServerApplicationContext context;
+	}
+}
